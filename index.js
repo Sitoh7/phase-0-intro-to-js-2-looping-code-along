fa63b29852bc797cc1949b28837bf55c3149015d
Sitoh7 <ha.wakhule@gmail.com>
2024-09-18 18:38:49 +0300

    use for and while loops

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solutions in this file
+
+
+function writeCards(names,eventName) {
+    
+    const newArray = [];
+    for(let i = 0; i < names.length; i++ )  {
+        newArray.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    
+      
+    }
+
+return newArray
+}
+
+function countDown(num) {
+    let i = num;
+    while(i>-1){
+       console.log(i);
+       
+    i--;
+      
+    }
+    return i
+}
+
+
+
+
+  
